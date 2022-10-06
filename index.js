311d52773fed0130dc6f62913511c118ea848712
nikc1603 <nikc1603@users.noreply.github.com>
2022-10-06 13:45:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+let resulthome = 0
+let resultguest = 0
+
+add1home = function(){
+   resulthome += 1
+   document.getElementById("welcome1").innerHTML=resulthome
+}
+add2home = function(){
+    resulthome += 2
+    document.getElementById("welcome1").innerHTML=resulthome
+ }
+ add3home = function(){
+    resulthome += 3
+    document.getElementById("welcome1").innerHTML=resulthome
+ }
+
+ add1guest = function(){
+    resultguest += 1
+    document.getElementById("welcome2").innerHTML=resultguest
+ }
+ add2guest = function(){
+    resultguest += 2
+    document.getElementById("welcome2").innerHTML=resultguest
+ }
+ add3guest = function(){
+    resultguest += 3
+    document.getElementById("welcome2").innerHTML=resultguest
+ }
